0914f4129ef530a6617c2dc136d90e0fda620812
he_is_him <ahmedayomide1002@gmail.com>
2025-08-08 11:19:01 +0100

    Update addcoins.js

--- commands/addcoins.js
+++ commands/addcoins.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const Database = require('../utils/database'); // Adjust path as needed
 
 // Initialize database
-const db = new Database();
+const db = require('../database/database');
 
 module.exports = {
   data: new SlashCommandBuilder()
